53fe7cee381ab1d510b95c5c9e2e0b9df1adc4c2
Kraji <krajenbrink.alexandre@hotmail.fr>
2017-07-23 14:05:39 -0600

    modification of the data processing script

--- data/data.py
+++ data/data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import cPickle
+import progressbar
 
 # load OHLC data from Kraken for ETHEUR
 
@@ -17,49 +18,56 @@ def ohlc(k, currency):
 	av_values = np.array([data[i][5] for i in range(nb_data)], dtype=float)
 	data_bin = np.array([(closing_values[i] - open_values[i] > 0.) for i in range(nb_data)], dtype=int)
 
-	return data,nb_data open_values, closing_values, closing_values, lowest_values, highest_values, av_values, data_bin
+	return data,nb_data, open_values, closing_values, closing_values, lowest_values, highest_values, av_values, data_bin
 
 # load all trade data from Kraken for ETHEUR
 
 def trade_data(k, currency, lastid):
-	res=k.query_public('Trades',{'pair':'currency','since':'lastid'})
+	res=k.query_public('Trades',{'pair':currency,'since':lastid})
 	#last= np.array(res['result']['last']).astype(float)
 	last= res['result']['last']
 	#np.savetxt('LastID.txt',[last])
-	data = np.array(res['result']['currency'])[:,:3].astype(float)
+	data = np.array(res['result'][currency])[:,:3].astype(float)
 
 	return last, data
 
 # load the id of the last transaction
 
-def last_transaction_id (k, currency):
-	res2=k.query_public('Trades',{'pair':'currency'})
+def last_transaction_id(k, currency):
+	res=k.query_public('Trades',{'pair':currency})
 	last_now=res['result']['last']
 
 	return last_now
 
 
 k=krakenex.API()
+currency='XETHZEUR'
 
 f = open('EthEur.p','rb')
 A = cPickle.load(f)
-A.shape
+print 'Shape of the transaction matrix : '+ str(A.shape)
 f.close()
 
 f=open('LastID','rb')
 last_id=f.read().decode('utf8')
 f.close()
-print last_id
+print 'ID of the last saved transaction : '+ last_id
+#print type(last_id)
 
 starttime=time.time()
 i=0
 
-last_now=last_transaction_id(k, XETHZEUR)
+last_now=last_transaction_id(k, currency)
+print 'ID of the last transation on the market : '+ last_now
 
 time.sleep(1. - ((time.time() - starttime) % 1.))
 
-while last_id<last_now or i<2000:
-    last_id, data= trade_data(k,XETHZEUR, last_id)
+bar = progressbar.ProgressBar(maxval=200, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar.start()
+
+while last_id<last_now or i<200:
+    last_id, data= trade_data(k,currency,last_id)
 
     f = open('EthEur.p','rb')
     A = cPickle.load(f)
@@ -69,11 +77,19 @@ while last_id<last_now or i<2000:
     A=np.concatenate((A,data),axis=0)
     cPickle.dump(A,f)
     f.close()
+
+    f = open('LastID','wb')
+    f.write(last_id)
+    f.close()
+
+    bar.update(i+1)
     i+=1
-    time.sleep(2. - ((time.time() - starttime) % 2.))
+    time.sleep(3. - ((time.time() - starttime) % 3.))
+
+bar.finish()
 
 f = open('LastID','wb')
-f.write(last_id,f)
+f.write(last_id)
 f.close()
 
 f = open('EthEur.p','rb')
