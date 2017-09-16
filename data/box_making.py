2ba10ea1ad5747b2c9b851f38198a41aec99bd6a
Kraji <krajenbrink.alexandre@hotmail.fr>
2017-09-16 17:04:57 +0200

    ajout de la variance dans le processing

--- data/box_making.py
+++ data/box_making.py
@@ -17,23 +17,36 @@ def box_making(data_input, delta_t, initial_time,final_time):
     time_interval = final_time - initial_time
     nb_bins = int(time_interval // delta_t +1)
     
-    data_compressed=np.zeros((nb_bins,3))
+    data_compressed=np.zeros((nb_bins,4))
     
     vwap = np.zeros(nb_bins)
     weights = np.zeros(nb_bins)
     time_stamp=initial_time+delta_t*np.arange(nb_bins)
+    variance=np.zeros(nb_bins)
 
     for j in range(nb_data):
     	ind = int((time[j]-initial_time)//delta_t)
-    	vwap[ind] += volume[j] * price[j]
-    	weights[ind] += volume[j]
+        if ind=>0:
+    	   vwap[ind] += volume[j] * price[j]
+    	   weights[ind] += volume[j]
     for k in range(len(vwap)):
     	if weights[k] > 0.00001:
             vwap[k] = vwap[k] / weights[k]
     	else:
     	    vwap[k] = vwap[k-1]
+    
+    for j in range(nb_data):
+        ind = int((time[j]-initial_time)//delta_t)
+        if ind=>0:
+           variance[ind] += volume[j] * (price[j]-vwap[ind])**2
+    for k in range(len(vwap)):
+        if weights[k] > 0.00001:
+            variance[k] = variance[k] / weights[k]
+        else:
+            variance[k] = variance[k-1]
 
     data_compressed[:,0]=vwap
     data_compressed[:,1]=weights
     data_compressed[:,2]=time_stamp
+    data_compressed[:,3]=variance
     return data_compressed
